a30b4aea1a3dd6193d528695111420eed4fef6a2
Annemarie Allen <annemarie@Annemaries-MacBook-Pro.local>
2020-04-16 15:51:01 -0800

    added 1/10

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 // Your code goes here
-//
-.. 
+
+
+// mouseover with 180 deg flip of h1 
+const logo = document.querySelector(".logo-heading");
+
+logo.addEventListener("mouseover", function(){
+    logo.style.transform="rotate(180deg)";
+})
+
+logo.addEventListener("mouseleave", function(){
+    logo.style.transform="scale(1)";
+})
+
+// keydown 
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((el)=>{
+    el.addEventListener("keydown", ()=>{
+        el.style.display="none";
+    })
+})
+
+
+// allPhotos.addEventListener('scroll', function(){
+//    allPhotos.style.animation = "mynewmove 4s 2";
+// })
